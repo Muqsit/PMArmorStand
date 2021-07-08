bdfa4a7863e78895aab6922aed76d253f565a3b1
Muqsit <hackhack05@gmail.com>
2021-07-08 12:04:13 +0000

    Reduce nested if conditions where possible by failing first for readability

--- src/muqsit/pmarmorstand/ArmorStandItem.php
+++ src/muqsit/pmarmorstand/ArmorStandItem.php
@@ -17,31 +17,31 @@ use pocketmine\player\Player;
 class ArmorStandItem extends Item{
 
 	public function onInteractBlock(Player $player, Block $blockReplace, Block $blockClicked, int $face, Vector3 $clickVector) : ItemUseResult{
-		if($blockClicked->isSolid()){
-			$pos = $blockClicked->getPos();
-			$world = $pos->getWorld();
-			$spawn_pos = $pos->addVector((new Vector3(0, 0, 0))->getSide($face))->add(0.5, 0.0, 0.5);
-			foreach($world->getNearbyEntities((new AxisAlignedBB(-0.5, 0.0, -0.5, 0.5, 1.0, 0.5))->offset($spawn_pos->x, $spawn_pos->y, $spawn_pos->z)) as $entity){
-				if($entity instanceof ArmorStandEntity){
-					return ItemUseResult::NONE();
-				}
-			}
-
-			$yaw = fmod($player->getLocation()->getYaw() + 180.0, 360.0); // inverted player yaw
-			$yaw = round($yaw / 45.0) * 45.0; // round to nearest 45.0
-
-			($ev = new PlayerPlaceArmorStandEvent($player, Location::fromObject($spawn_pos, $world, $yaw, 0.0)))->call();
-			if($ev->isCancelled()){
-				return ItemUseResult::NONE();
-			}
-
-			$entity = new ArmorStandEntity($ev->getLocation());
-			$entity->spawnToAll();
-
-			$this->pop();
-			return ItemUseResult::SUCCESS();
+		if(!$blockClicked->isSolid()){
+			return parent::onInteractBlock($player, $blockReplace, $blockClicked, $face, $clickVector);
 		}
 
-		return parent::onInteractBlock($player, $blockReplace, $blockClicked, $face, $clickVector);
+		$pos = $blockClicked->getPos();
+		$world = $pos->getWorld();
+		$spawn_pos = $pos->addVector((new Vector3(0, 0, 0))->getSide($face))->add(0.5, 0.0, 0.5);
+		foreach($world->getNearbyEntities((new AxisAlignedBB(-0.5, 0.0, -0.5, 0.5, 1.0, 0.5))->offset($spawn_pos->x, $spawn_pos->y, $spawn_pos->z)) as $entity){
+			if($entity instanceof ArmorStandEntity){
+				return ItemUseResult::NONE();
+			}
+		}
+
+		$yaw = fmod($player->getLocation()->getYaw() + 180.0, 360.0); // inverted player yaw
+		$yaw = round($yaw / 45.0) * 45.0; // round to nearest 45.0
+
+		($ev = new PlayerPlaceArmorStandEvent($player, Location::fromObject($spawn_pos, $world, $yaw, 0.0)))->call();
+		if($ev->isCancelled()){
+			return ItemUseResult::NONE();
+		}
+
+		$entity = new ArmorStandEntity($ev->getLocation());
+		$entity->spawnToAll();
+
+		$this->pop();
+		return ItemUseResult::SUCCESS();
 	}
 }
--- src/muqsit/pmarmorstand/Loader.php
+++ src/muqsit/pmarmorstand/Loader.php
@@ -39,28 +39,36 @@ final class Loader extends PluginBase{
 	protected function onEnable() : void{
 		SimplePacketHandler::createMonitor($this)->monitorIncoming(function(InventoryTransactionPacket $packet, NetworkSession $session) : void{
 			$player = $session->getPlayer();
-			if($player !== null){
-				$trData = $packet->trData;
-				if($trData instanceof UseItemOnEntityTransactionData && $trData->getActionType() === UseItemOnEntityTransactionData::ACTION_INTERACT){
-					$world = $player->getWorld();
-					$entity = $world->getEntity($trData->getEntityRuntimeId());
-					if($entity instanceof ArmorStandEntity){
-						$click_pos = $trData->getClickPos();
-						if($player->canInteract($click_pos, 8) && $entity->boundingBox->expandedCopy(0.25, 0.25, 0.25)->isVectorInside($click_pos)){
-							if($player->isSneaking()){
-								$old_pose = $entity->getPose();
-								$new_pose = ArmorStandPoseRegistry::instance()->next($old_pose);
-								$ev = new PlayerChangeArmorStandPoseEvent($entity, $old_pose, $new_pose, $player);
-								$ev->call();
-								if(!$ev->isCancelled()){
-									$entity->setPose($ev->getNewPose());
-								}
-							}else{
-								$this->behaviour_registry->get($player->getInventory()->getItemInHand())->handleEquipment($player, $entity, $click_pos);
-							}
-						}
-					}
+			if($player === null){
+				return;
+			}
+
+			$trData = $packet->trData;
+			if(!($trData instanceof UseItemOnEntityTransactionData) || $trData->getActionType() !== UseItemOnEntityTransactionData::ACTION_INTERACT){
+				return;
+			}
+
+			$world = $player->getWorld();
+			$entity = $world->getEntity($trData->getEntityRuntimeId());
+			if(!($entity instanceof ArmorStandEntity)){
+				return;
+			}
+
+			$click_pos = $trData->getClickPos();
+			if(!$player->canInteract($click_pos, 8) || !$entity->boundingBox->expandedCopy(0.25, 0.25, 0.25)->isVectorInside($click_pos)){
+				return;
+			}
+
+			if($player->isSneaking()){
+				$old_pose = $entity->getPose();
+				$new_pose = ArmorStandPoseRegistry::instance()->next($old_pose);
+				$ev = new PlayerChangeArmorStandPoseEvent($entity, $old_pose, $new_pose, $player);
+				$ev->call();
+				if(!$ev->isCancelled()){
+					$entity->setPose($ev->getNewPose());
 				}
+			}else{
+				$this->behaviour_registry->get($player->getInventory()->getItemInHand())->handleEquipment($player, $entity, $click_pos);
 			}
 		});
 	}
